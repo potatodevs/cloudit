b23ffca85e4f54a94672095821ff75f2da88177b
Yogesh Nagarur <yogeshnagarur@gmail.com>
2013-02-26 21:33:47 +0000

    It now accepts gmail aliases

--- src/com/tomasvitek/android/cloudapp/tools/EmailValidator.java
+++ src/com/tomasvitek/android/cloudapp/tools/EmailValidator.java
@@ -15,7 +15,7 @@ public class EmailValidator{
 	  private static Pattern pattern;
 	  private static Matcher matcher;
  
-	  private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	  private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*\\+[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
  
 	  /**
 	   * Validate hex with regular expression
