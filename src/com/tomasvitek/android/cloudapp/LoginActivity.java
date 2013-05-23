6b12537b730e7c3e65afd118c4dab9615b393ad4
Yogesh Nagarur <yogeshnagarur@gmail.com>
2013-05-23 13:56:38 +0100

    Back button and removed auto focus
    Pressing back button closes activity.

--- src/com/tomasvitek/android/cloudapp/LoginActivity.java
+++ src/com/tomasvitek/android/cloudapp/LoginActivity.java
@@ -18,6 +18,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,70 +28,67 @@ import com.tomasvitek.android.cloudapp.threads.LoginAsyncTask;
 import com.tomasvitek.android.cloudapp.tools.EmailValidator;
 
 public class LoginActivity extends BaseActivity {
-    
+
 	public boolean onCreateOptionsMenu(Menu menu) {
 		return true;
-    }
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-        getSupportActionBar().hide();
-        Button log = (Button) findViewById(R.id.loginButton);
-        log.setOnClickListener(new OnClickListener() {
-			
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		getSupportActionBar().hide();
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+		Button log = (Button) findViewById(R.id.loginButton);
+		findViewById(R.id.loginLayout).requestFocus();
+		log.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View arg0) {
 
-   			 ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-   			 NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-			     if (networkInfo != null && networkInfo.isConnected()) {
-						TextView utext = (TextView) findViewById(R.id.emailField);
-						TextView ptext = (TextView) findViewById(R.id.passwordField);
-						
-						String email = utext.getText().toString();
-						String password = ptext.getText().toString();
-						
-						if (EmailValidator.isValidEmail(email)) {				
-							if (!password.equals("")) {
-								
-								ProgressDialog dialog = ProgressDialog.show(LoginActivity.this, "", "Loggin' in...", true);
-								
-								String[] data = {email, password, "1"};
-								new LoginAsyncTask(LoginActivity.this, dialog).execute(data);						
-							}
-							else {
-								Toast.makeText(LoginActivity.this, "Password cannot be empty!", Toast.LENGTH_LONG).show();
-							}							
+				ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+				NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+				if (networkInfo != null && networkInfo.isConnected()) {
+					TextView utext = (TextView) findViewById(R.id.emailField);
+					TextView ptext = (TextView) findViewById(R.id.passwordField);
+
+					String email = utext.getText().toString();
+					String password = ptext.getText().toString();
+
+					if (EmailValidator.isValidEmail(email)) {
+						if (!password.equals("")) {
+
+							ProgressDialog dialog = ProgressDialog.show(LoginActivity.this, "",
+									"Loggin' in...", true);
+
+							String[] data = { email, password, "1" };
+							new LoginAsyncTask(LoginActivity.this, dialog).execute(data);
+						} else {
+							Toast.makeText(LoginActivity.this, "Password cannot be empty!",
+									Toast.LENGTH_LONG).show();
 						}
-						else {
-							Toast.makeText(LoginActivity.this, "Your email is not valid!", Toast.LENGTH_LONG).show();
-						}
-		  	     } else {
-		  	    	Toast.makeText(LoginActivity.this, "No internet connection!", Toast.LENGTH_LONG).show();
-		 	     }
-				
+					} else {
+						Toast.makeText(LoginActivity.this, "Your email is not valid!",
+								Toast.LENGTH_LONG).show();
+					}
+				} else {
+					Toast.makeText(LoginActivity.this, "No internet connection!", Toast.LENGTH_LONG)
+							.show();
+				}
+
 			}
 		});
-    }
-    
-    
-    @Override
-    public void onResume() {
-    	super.onResume();
-    	
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		String email = prefs.getString("email", "");
-		
+
 		if (!email.equals("")) {
-	        finish();
+			finish();
 		}
-    }    
-    
-    @Override
-    public void onBackPressed() {
-    	return; // TODO fix
-    }
-    
+	}
 }
